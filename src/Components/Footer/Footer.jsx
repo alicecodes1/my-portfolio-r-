aab68082a1efefb0b96e0d1309dd4e0f1ac3cb2e
ifeoluwa Seriki <seriki.ifeoluwa2017@uniabuja.edu.ng>
2024-10-20 01:14:14 +0100

    Update footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
       <div className="footer-top">
         <div className="footer-top-left">
           <img src={footer_logo} alt="" />
-          <p>I am an entry level developer from  Nigeria, with 2 Years experience.</p>
+          <p>I am an entry level developer from  Nigeria, with 1+ Years experience.</p>
         </div>
         <div className="footer-top-right">
             <div className="footer-email-input">
